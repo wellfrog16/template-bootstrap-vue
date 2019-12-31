5bc9521fe1559357fff2fa60b315b85318a19aa8
frog <wellfrog16@gmail.com>
2019-12-31 16:14:35 +0800

    清理目录

--- build/gh-pages.js
+++ build/gh-pages.js
@@ -3,12 +3,5 @@ const ghpages = require('gh-pages');
 ghpages.publish('dist', {
     message: 'Updates'
 }, () => {
-    ghpages.publish('docs', {
-        src: 'index.html',
-        message: 'Updates',
-        dest: 'docs',
-        add: true,
-    }, () => {
-        console.log('finished');
-    });
+    console.log('finished');
 });
